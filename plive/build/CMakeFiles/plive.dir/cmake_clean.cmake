1dd8ee38e616b3d66df6b96dceeebe9f39e8846f
Jibbo <posta.giovanni.defrancesco@gmail.com>
2013-05-06 12:04:20 +0000

    initial commit of jibbo

---
+++ plive/build/CMakeFiles/plive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/plive.dir/main.o"
+  "plive.pdb"
+  "plive"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/plive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
