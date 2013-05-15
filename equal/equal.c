892ead3dd7ef702bd7e2f0c1a29ba7dc2738a407
robzenn92 <rob.zenn92@hotmail.it>
2013-05-15 23:14:52 +0200

    update equal

--- equal/equal.c
+++ equal/equal.c
@@ -3,8 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <dirent.h>
 
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// header -----------------------------------------
+
+
+#define NAME_MAX 14   /* longest filename component; */
+                      /* system-dependent */
+
+#define MAX_PATH 1024
+
+typedef struct {              /* portable directory entry */
+   long ino;                  /* inode number */
+   char name[NAME_MAX+1];     /* name + '\0' terminator */
+} Dirent;
+
+typedef struct {              /* minimal DIR: no buffering, etc. */
+   int fd;                    /* file descriptor for the directory */
+   Dirent d;                  /* the directory entry */
+} DIR;
+
+DIR *opendir(char *dirname);
+Dirent *readdir(DIR *dfd);
+void closedir(DIR *dfd);
+void dirwalk(char *dir);
+
+// ------------------------------------------------
 
 struct str_file {
 
@@ -94,53 +121,102 @@ main (int argc, char **argv)
     for (index = optind; index < argc; index++)
         printf ("Non-option argument %s\n", argv[index]);
 
-
-
-/*
-    free(path1);
-    free(path2);
-
-    printf("\n\np1: %s\n", path1);
-    printf("\n\np2: %s\n", path2);
-*/
-
-    // File test for both filepath
-    /*
-    char* test1 = (char*) malloc(sizeof("test -d ") + sizeof(path1));
-    test1 = strcat(test1, "test -d ");
-    test1 = strcat(test1, path1);
-
-    char* test2 = (char*) malloc(sizeof("test -d ") + sizeof(path2));
-    test2 = strcat(test2, "test -d ");
-    test2 = strcat(test2, path2);
-
-    printf("\n\ntest1: %s\n", test1);
-    printf("\n\ntest2: %s\n", test2);
-    */
-
-/*
     // Start with project
     // .....................................................................................
 
-    // true is both path are files or directories!
-    // false if one path rappresent a file and the other rappresent a directory!
-    bool comparable = !system(test1) && !system(test2);
-    if(comparable) {
-        printf("Both paths are directories!\n");
 
-        // List all files directory 1
-        // ---------------------
-        // List all files directory 2
+    struct str_file * file1;
+    struct str_file * file2;
+    file1 = (struct str_file *) malloc(sizeof(struct str_file));
+    file2 = (struct str_file *) malloc(sizeof(struct str_file));
+
+    // Copy argv parameters into path1 and path2..
+    file1->path = (char*) malloc(sizeof(argv[1]));
+    file1->path = strcpy(file1->path, argv[1]);
+    file2->path = (char*) malloc(sizeof(argv[2]));
+    file2->path = strcpy(file2->path, argv[2]);
+
+    struct stat stbuf1;
+    struct stat stbuf2;
+
+
+    int access_err1 = (stat(file1->path, &stbuf1) == -1);
+    int access_err2 = (stat(file2->path, &stbuf2) == -1);
+
+    if ( access_err1 ) {
+        fprintf(stderr, "Error: can't access %s\n", file1->path);
+        return;
+    }
+    if ( access_err2) {
+        fprintf(stderr, "Error: can't access %s\n", file2->path);
+        return;
     }
 
-    // Directory test for both filepath
-    test1 = (char*) malloc(sizeof("test -d ") + sizeof(argv[1]));
-    test1 = strcpy(test1, "test -d ");
-    test1 = strcat(test1, path1);
-    test2 = (char*) malloc(sizeof("test -d ") + sizeof(argv[2]));
-    test2 = strcpy(test2, "test -d ");
-    test2 = strcat(test2, path2);
-*/
+    int is_dir_1 = ((stbuf1.st_mode & S_IFMT) == S_IFDIR);
+    int is_dir_2 = ((stbuf2.st_mode & S_IFMT) == S_IFDIR);
+
+
+    if(is_dir_1 ^ is_dir_2) {
+
+        // Same types (both paths are files or directories) ..
+        if(is_dir_1 && is_dir_2) {
+            // Directories case..
+            dirwalk(file1->path);
+            dirwalk(file2->path);
+        }
+        else {
+            // Files case..
+            printf("Both paths are files!\n");
+
+            if ((file1->file = fopen(file1->path,"rb")) == NULL)  {
+                // Impossibile aprire path1
+                perror(file1->path);
+                exit(EXIT_FAILURE);
+            }
+            else if ((file2->file = fopen(file2->path,"rb")) == NULL)  {
+                // Impossibile aprire path1
+                perror(file2->path);
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                // Obtain file1 dimension in bytes
+                fseek(file1->file, 0, SEEK_END);
+                file1->size = ftell(file1->file);
+                file1->line = malloc(file1->size);
+                fseek(file1->file, 0, SEEK_SET);  // Return to the top of the file
+
+                // Obtain file2 dimension in bytes
+                fseek(file2->file, 0, SEEK_END);
+                file2->size = ftell(file2->file);
+                file2->line = malloc(file2->size);
+                fseek(file2->file, 0, SEEK_SET);  // Return to the top of the file
+
+                printf("file1->path: %s\n", file1->path);
+                printf("file1->size: %d bytes\n", file1->size);
+                printf("file2->path: %s\n", file2->path);
+                printf("file2->size: %d bytes\n", file2->size);
+                printf("\n-----------------------------------\n");
+                
+                // Read the entire file1 and file2
+                file1->read = fread(file1->line, file1->size, 1, file1->file);
+                file2->read = fread(file2->line, file2->size, 1, file2->file);
+
+                printf("file1:\n\nRead: \n%s\n----------------------------", file1->line);
+                printf("file2:\n\nRead: \n%s\n----------------------------", file2->line);
+                free(file1->line);
+                free(file2->line);
+
+                fclose(file1->file);
+                fclose(file2->file);
+            }
+            exit(0);
+
+        }
+    }
+    else {
+        printf("You cannot compare two different kind of file (one directory one file)!\n");
+    }
 
 /*
     int len;
@@ -163,70 +239,28 @@ main (int argc, char **argv)
     closedir (pDir);
     */
 
-
-    if(comparable) {
-        printf("Both paths are files!\n");
-
-        struct str_file * file1;
-        struct str_file * file2;
-        file1 = (struct str_file *) malloc(sizeof(struct str_file));
-        file2 = (struct str_file *) malloc(sizeof(struct str_file));
-
-
-
-        // Copy argv parameters into path1 and path2..
-        file1->path = (char*) malloc(sizeof(argv[1]));
-        file1->path = strcpy(file1->path, argv[1]);
-        file2->path = (char*) malloc(sizeof(argv[2]));
-        file2->path = strcpy(file2->path, argv[2]);
-
-        if ((file1->file = fopen(file1->path,"rb")) == NULL)  {
-            // Impossibile aprire path1
-            perror(file1->path);
-            exit(EXIT_FAILURE);
-        }
-        else if ((file2->file = fopen(file2->path,"rb")) == NULL)  {
-            // Impossibile aprire path1
-            perror(file2->path);
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            // Obtain file1 dimension in bytes
-            fseek(file1->file, 0, SEEK_END);
-            file1->size = ftell(file1->file);
-            file1->line = malloc(file1->size);
-            fseek(file1->file, 0, SEEK_SET);  // Return to the top of the file
-
-            // Obtain file2 dimension in bytes
-            fseek(file2->file, 0, SEEK_END);
-            file2->size = ftell(file2->file);
-            file2->line = malloc(file2->size);
-            fseek(file2->file, 0, SEEK_SET);  // Return to the top of the file
-
-            printf("file1->path: %s\n", file1->path);
-            printf("file1->size: %d bytes\n", file1->size);
-            printf("file2->path: %s\n", file2->path);
-            printf("file2->size: %d bytes\n", file2->size);
-            printf("\n-----------------------------------\n");
-            
-            // Read the entire file1 and file2
-            file1->read = fread(file1->line, file1->size, 1, file1->file);
-            file2->read = fread(file2->line, file2->size, 1, file2->file);
-
-            printf("file1:\n\nRead: \n%s\n----------------------------", file1->line);
-            printf("file2:\n\nRead: \n%s\n----------------------------", file2->line);
-            free(file1->line);
-            free(file2->line);
-
-            fclose(file1->file);
-            fclose(file2->file);
-        }
-        exit(0);
-    }
-    else {
-        printf("You cannot compare two different kind of file!\n");
-    }
-
     return 0;
+}
+
+void dirwalk(char *dir)
+{
+    char name[MAX_PATH];
+    Dirent *dp;
+    DIR *dfd;
+
+    if ((dfd = opendir(dir)) == NULL) {
+        fprintf(stderr, "dirwalk: can't open %s\n", dir);
+        return;
+    }
+    while ((dp = readdir(dfd)) != NULL) {
+        if (strcmp(dp->name, ".") == 0 || strcmp(dp->name, ".."))
+            continue;    /* skip self and parent */
+        if (strlen(dir)+strlen(dp->name)+2 > sizeof(name))
+            fprintf(stderr, "dirwalk: name %s %s too long\n", dir, dp->name);
+        else {
+            sprintf(name, "%s/%s", dir, dp->name);
+            dirwalk(dp->name);
+        }
+    }
+    closedir(dfd);
 }
