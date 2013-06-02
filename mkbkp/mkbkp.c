b0e7dc69a5e1693434f8f6e5e086d0eff702d447
Nicola Perantoni <nicola.perantoni@gmail.com>
2013-06-02 11:40:04 +0200

    Refactoring

--- mkbkp/mkbkp.c
+++ mkbkp/mkbkp.c
@@ -1,14 +1,16 @@
-/*
-    Anno accademico     2012/2013
-    Corso studio        Informatica
-    Progetto            #1 MKBKB
-    Componenti:
-        Zen Roberto             151737
-        Giovanni De Francesco   152080
-        Perantoni Nicola        151709
-*/
-
-#include "mkbkp.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/dir.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <limits.h>
+#include <string.h>
 
 #define PATH_MAX_LENGTH 256
 
@@ -22,30 +24,31 @@ typedef struct {
 
 FILE* backup;
 
-char cwd[PATH_MAX_LENGTH];
-char subpath[PATH_MAX_LENGTH];
 char filevalue[PATH_MAX_LENGTH];
 char dirvalue[PATH_MAX_LENGTH];
-
-char *f_value = NULL;
-char *t_value = NULL;
+char cwd[PATH_MAX_LENGTH];
+char subpath[PATH_MAX_LENGTH];
 
 int f_flag = 0;
 int c_flag = 0;
 int x_flag = 0;
 int t_flag = 0;
 
+void manage();
+void printHelp();
+void makeBackup(char* path);
+void showBackupContent(char* archive);
+
 int main(int argc, char **argv) {
-	int c;
-	opterr = 0;
-
-	openlog(argv[0], LOG_CONS || LOG_PID, LOG_LOCAL0);
-
-	while ((c = getopt (argc, argv, "f:xct:")) != -1) {
+  int c;
+  opterr = 0;
+  
+  while ((c = getopt (argc, argv, "f:cxt")) != -1) {
     switch (c) {
       case 'f':
         f_flag = 1;
-        f_value = optarg;
+        strcpy(filevalue, optarg);
+        //strcpy(dirvalue, argv[optind]); 
         break;
       case 'c':
         c_flag = 1;
@@ -55,188 +58,176 @@ int main(int argc, char **argv) {
         break;
       case 't':
         t_flag = 1;
-        t_value = optarg;
         break;
+      case '?':
+        if (optopt == 'f') {
+          fprintf(stderr, "Option -%c requires the archive as an argument\n", optopt);
+        } else if (isprint (optopt)) {
+          fprintf(stderr, "Unknown option '-%c'\n", optopt);
+          printHelp();
+        } else {
+          fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
+        }
+        return 1;
+
       default:
-        printf("Argument not available\n");
-        printHelp();
-        exit(EXIT_FAILURE);
+        abort ();
     }
-    checkInput(optind, argc, argv);
+
+    manage();
   }
+
   return 0;
 }
 
-void checkInput(int opt_index, int argc, char** targets) {
+void manage() {
+  if(c_flag == 1 && (filevalue == NULL && dirvalue == NULL)) {
+    printf("You must use the -f flag to specify the archive you want to create\n");
 
-	FILE *tempfile;
+  } else if((c_flag == 1 && f_flag == 1) && (filevalue != NULL && dirvalue != NULL)) {
+    // Crea l'archivio
+    printf("Creo l'archivio chiamato: %s della cartella: %s\n", filevalue, dirvalue);
+    makeBackup(dirvalue);
+  } else if(x_flag == 1 && (filevalue == NULL && dirvalue == NULL)) {
+    printf("You must use the -f flag to specify the archive you want to extract\n");
 
-	// 
-	// Visualizzazione del contenuto di un archivio
-	// 
-	if(t_flag == 1) {
-		if(c_flag == 1 || x_flag == 1 || f_flag == 1) {
-			printf("Errore nell'utilizzo dei parametri\n");
-		} else if (t_value == NULL) {
-			printf("Non è stato specificato un archivio\n");
-		} else {
-			printf("Visualizzo il contenuto archivio: %s\n", optarg);
-			if(tempfile = fopen(f_value, "r")) {
-				extractBackup(tempfile);
-			}
-		}
-	}
+  } else if((x_flag == 1 && f_flag == 1) && (filevalue != NULL && dirvalue != NULL)) {
+    // Estrae l'archivio
+    printf("Estraggo l'archivio: %s\n", filevalue);
 
-	// 
-	// Estrazione di un archivio
-	// 
-	if(x_flag == 1) {
-		if(f_flag == 0) {
-			printf("Errore nell'utilizzo dei parametri\n");
-		} else if (f_value == NULL) {
-			printf("Non è stato specificato un archivio\n");
-		} else if (c_flag == 1){
-			printf("\n");
-		} else {
-			printf("Estrazione archivio\n");
-			if(tempfile = fopen(f_value, "r")) {
-				extractBackup(tempfile);
-			} else {
-				printf("File non trovato\n");
-			}	
-		}
-	}
+  } else if(t_flag == 1 && (filevalue == NULL && dirvalue == NULL)) {
+    printf("You must use the -f flag to specify the archive you want to analyze\n");
 
-	// 
-	// Creazione di un nuovo archivio
-	// 
-	if(c_flag == 1) {
-		if(f_flag == 0){
-			printf("Errore nell'utilizzo dei parametri\n");
-		} else if (f_value == NULL) {
-			printf("Non è stato specificato un archivio\n");
-		} else {
-			printf("Compressione archivio\n");
-			createBackup();
-		}
-	}
-
-	if(f_flag == 1) {
-		printf("Non è stato specificato un archivio\n");
-	} else {
-		printHelp();
-	}
+  } else if ((t_flag == 1 && f_flag == 1) && filevalue != NULL) {
+    // Mostra il contenuto dell'archivio
+    showBackupContent(filevalue);
+  }
 }
 
 void printHelp() {
-	printf("Usage: mkbkp [-c] [-x] [-t] [-f]\n");
-	printf("\t -f to create or extract an archive");
-	printf("\n\t -c to create a new archive");
-	printf("\n\t -x to extract an archive in the current directory");
-	printf("\n\t -t to display the content of an archive\n");
+  printf("Usage: mkbkp [-c] [-x] [-t] [-f]\n");
+  printf("\t -f to create or extract an archive");
+  printf("\n\t -c to create a new archive");
+  printf("\n\t -x to extract an archive in the current directory");
+  printf("\n\t -t to display the content of an archive\n");
 }
 
-void createBackup(char* path) {
-	struct dirent* direntry;
-	DIR* dir;
-	struct stat stbuf;
-	int is_dir;
-	int access_err;
+void makeBackup(char* path) {
+  struct dirent* direntry;
+  DIR* dir;
+  struct stat stbuf;
+  int is_dir;
+  int access_err;
 
-	FILE *openedfile;
-	char* buffer = NULL;
-	int flength = 0;
+  FILE *openedfile;
+  char* buffer = NULL;
+  int flength = 0;
 
-	// La funzione opendir apre un directory stream.
-	// Restituisce un puntatore ad un oggetto di tipo DIR in caso di successo e NULL in caso di errore.
-	// Inoltre posiziona lo stream sulla prima voce contenuta nella directory.
-	if ((dir = opendir(path)) == NULL) { 
-	  printf("Non è stato possibile aprire la cartella %s\n", path);
-	  return;
-	}
+  // La funzione opendir apre un directory stream.
+  // Restituisce un puntatore ad un oggetto di tipo DIR in caso di successo e NULL in caso di errore.
+  // Inoltre posiziona lo stream sulla prima voce contenuta nella directory.
+  if ((dir = opendir(path)) == NULL) { 
+      printf("scanworkingdir: can't open %s\n", path);
+      return;
+  }
 
-	// La funzione readdir legge la voce corrente nella directory, posizionandosi sulla voce successiva.
-	// Restituisce un puntatore al directory stream in caso di successo e NULL in caso di errore.
-	// Loop on directory entries
-	while ((direntry = readdir(dir)) != NULL) {
-		if (strcmp(direntry -> d_name, ".") == 0 || strcmp(direntry -> d_name, "..") == 0) {
-		  continue;
-		}
+  // La funzione readdir legge la voce corrente nella directory, posizionandosi sulla voce successiva.
+  // Restituisce un puntatore al directory stream in caso di successo e NULL in caso di errore.
+  // Loop on directory entries
+  while ((direntry = readdir(dir)) != NULL) {
+    if (strcmp(direntry -> d_name, ".") == 0 || strcmp(direntry -> d_name, "..") == 0) {
+      continue;
+    }
 
-		size_t length = strlen(path) + strlen(direntry -> d_name) + 2;
-		char * concat = (char *) malloc(sizeof(char) * length);
-		snprintf(concat, length, "%s/%s", path, direntry -> d_name);
-		stat(concat, &stbuf);
+    size_t length = strlen(path) + strlen(direntry -> d_name) + 2;
+    char * concat = (char *) malloc(sizeof(char) * length);
+    snprintf(concat, length, "%s/%s", path, direntry -> d_name);
+    stat(concat, &stbuf);
 
-		// Legge la cartella corrente
-		getcwd(cwd, PATH_MAX_LENGTH);
-		strcat(cwd, "/");
-		strcat(cwd, filevalue);
+    // Legge la cartella corrente
+    getcwd(cwd, PATH_MAX_LENGTH);
+    strcat(cwd, "/");
+    strcat(cwd, filevalue);
 
-		// Apro il file di backup in Append
-		backup = fopen(cwd, "a+");
-		if(backup == NULL) {
-		  perror(cwd);
-		  exit(EXIT_FAILURE);
-		}
+    // Apro il file di backup in Append
+    backup = fopen(cwd, "a+");
+    if(backup == NULL) {
+      perror(cwd);
+      exit(EXIT_FAILURE);
+    }
 
-		// Bool che indica se è una directory
-		is_dir = ((stbuf.st_mode & S_IFMT) == S_IFDIR);
+    // Bool che indica se è una directory
+    is_dir = ((stbuf.st_mode & S_IFMT) == S_IFDIR);
 
-		// se è una directory
-		if (is_dir) {
-			strcpy(subpath, path);
-			strcat(subpath, "/");
-			strcat(subpath, direntry -> d_name);
+    // se è una directory
+    if (is_dir) {
+      strcpy(subpath, path);
+      strcat(subpath, "/");
+      strcat(subpath, direntry -> d_name);
 
-			fprintf(backup, "\nDIR=");
-			fprintf(backup, "%s", subpath);
-			fprintf(backup, "\n");
+      fprintf(backup, "\nDIR=");
+      fprintf(backup, "%s", subpath);
+      fprintf(backup, "\n");
 
-			createBackup(subpath);
-		} else {
-			// allora è un file
-			str_file* temp;
+      makeBackup(subpath);
+    } else {
+      // allora è un file
+      str_file* temp;
 
-			temp = (str_file* ) malloc(sizeof(str_file));
-			strcpy(temp -> path, path);
-			strcat(temp -> path, "/");
-			strcat(temp -> path, direntry -> d_name);
+      temp = (str_file* ) malloc(sizeof(str_file));
+      strcpy(temp -> path, path);
+      strcat(temp -> path, "/");
+      strcat(temp -> path, direntry -> d_name);
 
-			temp -> file = fopen(temp -> path, "rb");
-			if(temp -> file == NULL) {
-				perror(temp -> path);
-				exit(EXIT_FAILURE);
-			}
+      temp -> file = fopen(temp -> path, "rb");
+      if(temp -> file == NULL) {
+        perror(temp -> path);
+        exit(EXIT_FAILURE);
+      }
 
-			fseek(temp -> file, 0, SEEK_END);
-			temp -> size = ftell(temp -> file);
-			fseek(temp -> file, 0, SEEK_SET);
+      fseek(temp -> file, 0, SEEK_END);
+      temp -> size = ftell(temp -> file);
+      fseek(temp -> file, 0, SEEK_SET);
 
-			temp -> line = (char* ) malloc(temp -> size);
-			temp -> read = fread(temp -> line, temp -> size, 1, temp -> file);
+      temp -> line = (char* ) malloc(temp -> size);
+      temp -> read = fread(temp -> line, temp -> size, 1, temp -> file);
 
-			// Scrive il delimitatore del file all'interno dell'archivio
-			fprintf(backup, "FILE=");
-			fprintf(backup, "%s", temp -> path);
-			fprintf(backup, "\n");
+      // Scrive il delimitatore del file all'interno dell'archivio
+      fprintf(backup, "FILE=");
+      fprintf(backup, "%s", temp -> path);
+      fprintf(backup, "\n");
 
-			// Scrive il file appena letto all'interno dell'archivio
-			fwrite(temp->line, 1, temp -> size, backup);
-			fclose(temp -> file);
+      // Scrive il file appena letto all'interno dell'archivio
+      fwrite(temp->line, 1, temp -> size, backup);
+      fclose(temp -> file);
 
-			free(temp -> line);
-			free(temp);
-		}
-		fclose(backup);
-	}
-	closedir(dir);
+      free(temp -> line);
+      free(temp);
+    }
+
+    fclose(backup);
+  }
+
+  closedir(dir);
 }
 
-void extractBackup(FILE* bkp)  {
-	
-}
+void showBackupContent(char* archive) {
+  char workingdir[PATH_MAX_LENGTH];
+  getcwd(workingdir, PATH_MAX_LENGTH);
+  strcat(workingdir, "/");
+  strcat(workingdir, archive);
 
-void showBackupContent(FILE* bkp) {
-	
+  char buff[PATH_MAX_LENGTH];
+
+  FILE* archivetoshow = fopen(workingdir, "r");
+  if(archivetoshow == NULL) {
+    perror(workingdir);
+    exit(EXIT_FAILURE);
+  } else {
+    while(fgets(buff, PATH_MAX_LENGTH, archivetoshow) != NULL) {
+      printf("%s\n", buff);
+    }
+  }
+
+  fclose(archivetoshow);
 }
